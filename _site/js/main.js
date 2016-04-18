3ce3d7c9e57d008e59aa75f654c5ab2398f52a69
mcneela86 <mcneela86@gmail.com>
2016-04-18 12:33:54 +1000

    Added Jekyll

---
+++ _site/js/main.js
@@ -0,0 +1,24 @@
+//	HISTORICIZE AND AJAXIFY OUR SITE
+jQuery(document).ready(function() {
+	var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+
+	//	Catch all internally-focused links and push a new state.
+	//	Note: External links will not be affected by this behavior.
+	$(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+		e.preventDefault();
+		History.pushState({}, "", this.pathname);
+	});
+
+	History.Adapter.bind(window, 'statechange', function(){
+		var State = History.getState();
+		$.get(State.url, function(data){	// Use AJAX to get the new content.
+			document.title = data.match(/<title>(.*?)<\/title>/)[1];
+			$('.content').html($(data).find('.content')); 	// Pull the post we want out of the .content class.
+															// If you change the class of the post container,
+															// you must change it here!!!
+			// _gaq.push(['_trackPageview', State.url]);	// This updates Google Analytics with a visit to the new page.
+														// If you don't use Google Analytics, you can safety comment or
+														// remove that line.
+		});
+	});
+});
